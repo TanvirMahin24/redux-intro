60d9eef508f6af42cd7d079b542e45f1bddb4dd3
TanvirMahin24 <tanvirmahin24@gmail.com>
2021-02-10 20:56:16 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import CovidStats from "./CovidStats";
 const App = () => {
--- src/CovidStats.js
+++ src/CovidStats.js
@@ -30,20 +30,25 @@ const CovidStats = () => {
   return (
     <div style={{ paddingBottom: "80px" }}>
       <h1>COVID status</h1>
-      <div className="d-flex">
-        <span className="pr">
-          <b>Total Confirmed:</b> {stat.totalConfirmed}
-        </span>
-        <span className="pr">
-          <b>New Confirmed:</b> {stat.newConfirmed}
-        </span>
-        <span className="pr">
-          <b>Total Death:</b> {stat.totalDeath}
-        </span>
-        <span className="pr">
-          <b>New Death:</b> {stat.newDath}
-        </span>
-      </div>
+
+      {stat.totalConfirmed == 0 ? (
+        <span>Loading...</span>
+      ) : (
+        <div className="d-flex">
+          <span className="pr">
+            <b>Total Confirmed:</b> {stat.totalConfirmed}
+          </span>
+          <span className="pr">
+            <b>New Confirmed:</b> {stat.newConfirmed}
+          </span>
+          <span className="pr">
+            <b>Total Death:</b> {stat.totalDeath}
+          </span>
+          <span className="pr">
+            <b>New Death:</b> {stat.newDath}
+          </span>
+        </div>
+      )}
     </div>
   );
 };
