636ab01f7f8fe8bb1ba36c96e8d63c09941da738
TanvirMahin24 <tanvirmahin24@gmail.com>
2021-02-10 19:49:03 +0600

    simple project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [counter, setCounter] = useState(0);
+  const increment = (val = 1) => setCounter(counter + val);
+  const decrement = (val = 1) => setCounter(counter - val);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter : {counter}</h1>
+      <div style={{ display: "block" }}>
+        <button onClick={() => increment()}>Increment +1</button>
+        <button onClick={() => increment(5)}>Increment +5</button>
+        <button onClick={() => decrement()}>Decrement -1</button>
+        <button onClick={() => decrement(5)}>Decrement -5</button>
+      </div>
     </div>
   );
 }
